c38206bbe027a26dc99a8870a22f2753ce02ec75
thirkellian <thirkellian@gmail.com>
2015-10-31 20:15:53 -0500

    Add 'Remember me' option with checkbox

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,13 @@ protect_from_forgery with: :exception
 private
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find_by_auth_token!(cookies[:auth_token]) if cookies[:auth_token]
   end
   helper_method :current_user
 
-  def authorize
+  def authorize # Says you have to be logged in before you can do stuff
+    # so you'd use it as a 'before filter' on anything you want users to be logged in for.
+    # Could use a gem like 'CanCan' to make specific authorization for different actions.
     redirect_to login_url, alert: "Not authorized" if current_user.nil?
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :email, uniqueness: true
+
+  before_create { generate_token(:auth_token) }
+
+  def generate_token(column)
+    begin
+      self[column] = SecureRandom.base64.tr("+/", "-_")
+    end while User.exists?(column => self[column])
+  end
 end
